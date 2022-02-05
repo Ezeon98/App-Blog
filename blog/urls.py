e4f2207e08962a75ce6e1d326167d2f19f6a9756
Ezeon98 <ezeon3@gmail.com>
2022-02-05 16:28:52 -0300

    Agrego el DELETE

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BlogListView, BlogCreateView, DetailPostView, BlogUpdateView
+from .views import BlogListView, BlogCreateView, DetailPostView, BlogUpdateView, BlogDeleteView
 
 app_name = "blog"
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('inicio/', BlogListView.as_view(), name='home'),
     path('create/', BlogCreateView.as_view(), name='create'),
     path('<int:pk>/', DetailPostView.as_view(), name='detail'), ## <int:pk> hace referencia al ID del modelo
-    path('<int:pk>/update/', BlogUpdateView.as_view(), name='update')
+    path('<int:pk>/update/', BlogUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', BlogDeleteView.as_view(), name='delete')
 ]
