27ede14efd5723f85a483e12470c275ab424c62a
Ezeon98 <ezeon3@gmail.com>
2022-02-05 16:21:31 -0300

    Agrego el EDIT

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import BlogListView, BlogCreateView
+from .views import BlogListView, BlogCreateView, DetailPostView, BlogUpdateView
 
 app_name = "blog"
 
 urlpatterns = [
     path('inicio/', BlogListView.as_view(), name='home'),
     path('create/', BlogCreateView.as_view(), name='create'),
-
+    path('<int:pk>/', DetailPostView.as_view(), name='detail'), ## <int:pk> hace referencia al ID del modelo
+    path('<int:pk>/update/', BlogUpdateView.as_view(), name='update')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,22 @@
-from django.shortcuts import render, redirect
-from django.views.generic import View
+from django.shortcuts import get_object_or_404, render, redirect
+from django.views.generic import View, UpdateView
 from .forms import PostCreateForm
 from .models import Post
+from django.urls import reverse_lazy
 
 
 class BlogListView(View):
     def get(self,request, *args, **kwargs):
+        posts = Post.objects.all()
         context = {
-            
+            'posts': posts
         }
         return render(request, 'blog_list.html', context)
 
 
-class BlogCreateView(View):
-#Metodo Get
+class BlogCreateView(View):  
+
+    #Metodo Get
     def get(self,request, *args, **kwargs):
         form=PostCreateForm()
         context = {
@@ -22,7 +25,7 @@ class BlogCreateView(View):
         return render(request, 'blog_create.html', context)
 
 
-#Metodo de Post
+    #Metodo de Post
     def post(self,request, *args, **kwargs):
         if request.method == 'POST':
             form=PostCreateForm(request.POST)
@@ -39,3 +42,19 @@ class BlogCreateView(View):
                 
             }
             return render(request, 'blog_create.html', context)
+
+class DetailPostView(View):
+    def get (self,request,pk, *args,**kwargs):
+        post = get_object_or_404(Post, pk=pk)
+        context={
+            'post':post
+        }
+        return render (request, 'blog_detail.html', context)
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields=['title', 'content']
+    template_name ='blog_update.html'
+    def get_success_url(self):
+        pk = self.kwargs['pk']
+        return reverse_lazy('blog:detail', kwargs={'pk':pk})
